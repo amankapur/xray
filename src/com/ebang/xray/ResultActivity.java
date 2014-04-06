0271cf479f1dc1a4518965abd9cb5a93f2e1b405
Aman Kapur <amankapur91@gmail.com>
2014-04-06 14:28:27 -0400

    parse added

--- src/com/ebang/xray/ResultActivity.java
+++ src/com/ebang/xray/ResultActivity.java
@@ -3,6 +3,8 @@ package com.ebang.xray;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * Created by amankapur91 on 4/6/14.
@@ -23,7 +25,11 @@ public class ResultActivity extends Activity {
         code = intent.getStringExtra("code");
         codeType = intent.getStringExtra("type");
 
+        Parse.initialize(this, "AsSpaxVT7WnJqQak0I5OJo4qh30qWDuko1DIITEG", "SjDfZt7x3mmmqbmlk4YGJVSuauV6TgJC4hoV04eo");
 
+        ParseObject testObject = new ParseObject("a");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
 
     }
 }
